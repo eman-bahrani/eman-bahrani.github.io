84954df0eb30bb6e07815019a974cfb635a4c3ee
Eman AlBahrani <eman.bahrani97@gmail.com>
2025-01-01 17:15:48 +0300

    greeting text + sm and cv links

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,24 +21,24 @@ const illustration = {
 
 const greeting = {
   username: "Eman",
-  title: "Hi all, I'm Eman",
+  title: "Welcome! I'm Eman",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "I am a bioinformatician and public health advocate passionate about leveraging computational tools to prevent and mitigate disease. With expertise in data analytics, research, and policy, I am driven to advance precision public health. Explore my work in biomarkers, transcriptional pathways, and health initiatives, including my platform for precision public health."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/15_zuGa-lPBUDO5dWVathj8ZOqvsymaBl/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
+  github: "https://github.com/eman-bahrani",
+  linkedin: "http://linkedin.com/in/emanbahrani/",
+  gmail: "eman.bahrani97@gmail.com",
+  gitlab: "https://gitlab.com/saadpasta", 
   facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
+  medium: "https://medium.com/@saadpasta", 
   stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
