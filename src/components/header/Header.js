40ca4249970e849c8f6243578c97c42c500abf6b
Eman AlBahrani <eman.bahrani97@gmail.com>
2025-01-07 21:27:32 +0300

    updated contact me in header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -77,7 +77,7 @@ function Header() {
             </li>
           )} */}
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact">Get In Touch</a>
           </li>
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
