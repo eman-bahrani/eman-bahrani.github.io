46f8473688bc053952e16342eda3771aca009d6b
Eman AlBahrani <eman.bahrani97@gmail.com>
2025-01-07 15:24:23 +0300

    updated proficiency in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -139,17 +139,25 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
-    },
-    {
-      Stack: "Backend",
-      progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
+      Stack: "Data Analysis & Bioinformatics",
+      progressPercentage: "90%"
+     },
+     {
+      Stack: "Statistical Analysis",
+      progressPercentage: "88%"
+     },
+     {
+      Stack: "Project Management",
+      progressPercentage: "85%"
+     },
+     {
+      Stack: "Strategic Planning",
+      progressPercentage: "80%"
+     },
+     {
+      Stack: "Public Speaking",
+      progressPercentage: "75%"
+     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
