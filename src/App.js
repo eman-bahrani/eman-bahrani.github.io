d14dfe4efec05b2ce13e96c4e5fe9fc828fe9adc
Ali-Albahrani <alialbahrani3@gmail.com>
2025-02-19 23:23:57 +0000

    use hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./containers/Main";
 import BlogPosts from "./containers/BlogPosts/BlogPosts";
 import BlogDetail from "./containers/BlogDetail/BlogDetail"; 
 
 function App() {
   return (
-    <Router basename="/eman-bahrani">
+    <Router>
       <div className="App">
         <Switch>
           <Route exact path="/" component={Main} />
