a221c02cb2ed3aaf9e295f1be122fbc99e55cb96
Ali-Albahrani <alialbahrani3@gmail.com>
2025-02-20 00:24:58 +0000

    add import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Main from "./containers/Main";
 import BlogPosts from "./containers/BlogPosts/BlogPosts";
 import BlogDetail from "./containers/BlogDetail/BlogDetail"; 
